237928e4401c82bd4864b881f81e63474b08cda7
TotallyMehis <totally.mehis@gmail.com>
2017-08-23 19:44:56 +0300

    Send prop optimizations

--- mp/src/game/shared/zmr/zmr_playerlocaldata.cpp
+++ mp/src/game/shared/zmr/zmr_playerlocaldata.cpp
@@ -8,12 +8,12 @@
 
 #ifndef CLIENT_DLL
 BEGIN_SEND_TABLE_NOBASE( CZMPlayerLocalData, DT_ZM_PlyLocal )
-    SendPropInt( SENDINFO( m_nResources ) ),
+    SendPropInt( SENDINFO( m_nResources ), -1, SPROP_UNSIGNED ),
     SendPropFloat( SENDINFO( m_flFlashlightBattery ), 10, SPROP_UNSIGNED | SPROP_ROUNDUP, 0.0f, 100.0f ),
 END_SEND_TABLE()
 #else
 BEGIN_RECV_TABLE_NOBASE( CZMPlayerLocalData, DT_ZM_PlyLocal )
-    RecvPropInt( RECVINFO( m_nResources ) ),
+    RecvPropInt( RECVINFO( m_nResources ), SPROP_UNSIGNED ),
     RecvPropFloat( RECVINFO( m_flFlashlightBattery ), SPROP_UNSIGNED | SPROP_ROUNDUP ),
 END_RECV_TABLE()
 #endif
