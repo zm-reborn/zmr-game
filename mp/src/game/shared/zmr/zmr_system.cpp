dff530dd98fdea12b4c713c257b7521ab1070113
TotallyMehis <totally.mehis@gmail.com>
2017-08-24 11:32:05 +0300

    Clear decals & ragdolls on round restart

--- mp/src/game/shared/zmr/zmr_system.cpp
+++ mp/src/game/shared/zmr/zmr_system.cpp
@@ -1,9 +1,10 @@
 #include "cbase.h"
+#include "GameEventListener.h"
 
 #include "zmr/zmr_web.h"
 
 
-class CZMSystem : public CAutoGameSystem
+class CZMSystem : public CAutoGameSystem, public CGameEventListener
 {
 public:
     CZMSystem() : CAutoGameSystem( "ZMSystem" )
@@ -11,6 +12,9 @@ public:
     }
 
     virtual void PostInit() OVERRIDE;
+
+
+    virtual void FireGameEvent( IGameEvent* pEvent ) OVERRIDE;
 };
 
 void CZMSystem::PostInit()
@@ -22,7 +26,38 @@ void CZMSystem::PostInit()
 #endif
 
 
+#ifdef CLIENT_DLL
+    ListenForGameEvent( "round_restart_post" );
+#endif
+
     g_pZMWeb->QueryVersionNumber();
+}
+
+void CZMSystem::FireGameEvent( IGameEvent* pEvent )
+{
+#ifdef CLIENT_DLL
+    if ( Q_strcmp( pEvent->GetName(), "round_restart_post" ) == 0 )
+    {
+        DevMsg( "Client received round restart event!\n" );
+
+
+        // Clear decals.
+        engine->ClientCmd( "r_cleardecals" );
+
+
+        // Remove client ragdolls since they don't like getting removed.
+        C_ClientRagdoll* pRagdoll;
+        for ( C_BaseEntity* pEnt = ClientEntityList().FirstBaseEntity(); pEnt; pEnt = ClientEntityList().NextBaseEntity( pEnt ) )
+        {
+            pRagdoll = dynamic_cast<C_ClientRagdoll*>( pEnt );
+            if ( pRagdoll )
+            {
+                // This will make them fade out.
+                pRagdoll->SUB_Remove();
+            }
+        }
+    }
+#endif
 }
 
 CZMSystem g_ZMSystem;
