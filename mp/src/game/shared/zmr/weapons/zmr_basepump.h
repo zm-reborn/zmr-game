16dca4627a6359206fcbfdd6f964736bc37fb01f
TotallyMehis <totally.mehis@gmail.com>
2017-08-27 14:09:38 +0300

    Added start/finish reload anims + moved rifle & shotgun to base pump weapon

---
+++ mp/src/game/shared/zmr/weapons/zmr_basepump.h
@@ -0,0 +1,41 @@
+#pragma once
+
+
+#include "zmr_base.h"
+
+
+#ifdef CLIENT_DLL
+#define CZMBasePumpWeapon C_ZMBasePumpWeapon
+#endif
+
+
+#define RELOADSTATE_NONE        0
+#define RELOADSTATE_START       1
+#define RELOADSTATE_RELOADING   2
+
+class CZMBasePumpWeapon : public CZMBaseWeapon
+{
+public:
+    DECLARE_CLASS( CZMBasePumpWeapon, CZMBaseWeapon );
+    DECLARE_NETWORKCLASS(); 
+    DECLARE_PREDICTABLE();
+
+
+    CZMBasePumpWeapon();
+
+
+    virtual bool Holster( CBaseCombatWeapon* pSwitchTo ) OVERRIDE;
+    virtual bool Reload( void ) OVERRIDE;
+    virtual void ItemPostFrame( void ) OVERRIDE;
+    virtual void FinishReload( void ) OVERRIDE;
+
+    virtual Activity GetReloadStartAct() { return ACT_VM_RELOAD_START;};
+    virtual Activity GetReloadEndAct() { return ACT_VM_RELOAD_FINISH; };
+    virtual Activity GetPumpAct() { return ACT_SHOTGUN_PUMP; };
+    virtual void StartReload( void );
+    virtual void Pump();
+
+protected:
+    CNetworkVar( bool, m_bNeedPump );
+    CNetworkVar( int, m_iReloadState );
+};
--- mp/src/game/shared/zmr/weapons/zmr_shotgun.cpp
+++ mp/src/game/shared/zmr/weapons/zmr_shotgun.cpp
@@ -4,25 +4,21 @@
 #include "items.h"
 #endif
 
-#include "zmr/zmr_shareddefs.h"
-#include "zmr_base.h"
-
-
-#include "zmr/zmr_player_shared.h"
+#include "zmr_basepump.h"
 
 
 #ifdef CLIENT_DLL
 #define CZMWeaponShotgun C_ZMWeaponShotgun
 #endif
 
-class CZMWeaponShotgun : public CZMBaseWeapon
+class CZMWeaponShotgun : public CZMBasePumpWeapon
 {
 public:
-	DECLARE_CLASS( CZMWeaponShotgun, CZMBaseWeapon );
-	DECLARE_NETWORKCLASS(); 
-	DECLARE_PREDICTABLE();
+    DECLARE_CLASS( CZMWeaponShotgun, CZMBasePumpWeapon );
+    DECLARE_NETWORKCLASS();
+    DECLARE_PREDICTABLE();
 #ifndef CLIENT_DLL
-	DECLARE_ACTTABLE();
+    DECLARE_ACTTABLE();
 #endif
 
     CZMWeaponShotgun();
@@ -46,36 +42,26 @@ public:
         if ( !pPlayer ) return;
 
 
-	    pPlayer->ViewPunch( QAngle( random->RandomFloat( -2, -1 ), random->RandomFloat( -2, 2 ), 0 ) );
+        pPlayer->ViewPunch( QAngle( random->RandomFloat( -2, -1 ), random->RandomFloat( -2, 2 ), 0 ) );
     }
     
     virtual float GetFireRate( void ) OVERRIDE { return 0.55f; };
 
-
-
-    virtual bool Reload( void ) OVERRIDE;
     virtual void PrimaryAttack( void ) OVERRIDE;
-    virtual void ItemPostFrame( void ) OVERRIDE;
 
-    virtual void Pump();
 
-protected:
-    CNetworkVar( bool, m_bNeedPump ); // When emptied completely
+    virtual Activity GetReloadStartAct() OVERRIDE { return ACT_SHOTGUN_RELOAD_START; };
+    virtual Activity GetReloadEndAct() OVERRIDE { return ACT_SHOTGUN_RELOAD_FINISH; };
+    virtual Activity GetPumpAct() OVERRIDE { return ACT_SHOTGUN_PUMP; };
 };
 
 IMPLEMENT_NETWORKCLASS_ALIASED( ZMWeaponShotgun, DT_ZM_WeaponShotgun )
 
 BEGIN_NETWORK_TABLE( CZMWeaponShotgun, DT_ZM_WeaponShotgun )
-#ifdef CLIENT_DLL
-    RecvPropBool( RECVINFO( m_bNeedPump ) ),
-#else
-    SendPropBool( SENDINFO( m_bNeedPump ) ),
-#endif
 END_NETWORK_TABLE()
 
 #ifdef CLIENT_DLL
 BEGIN_PREDICTION_DATA( CZMWeaponShotgun )
-    DEFINE_PRED_FIELD( m_bNeedPump, FIELD_BOOLEAN, FTYPEDESC_INSENDTABLE ),
 END_PREDICTION_DATA()
 #endif
 
@@ -85,14 +71,14 @@ PRECACHE_WEAPON_REGISTER( weapon_zm_shotgun );
 #ifndef CLIENT_DLL
 acttable_t CZMWeaponShotgun::m_acttable[] = 
 {
-	{ ACT_HL2MP_IDLE,					ACT_HL2MP_IDLE_SHOTGUN,					false },
-	{ ACT_HL2MP_RUN,					ACT_HL2MP_RUN_SHOTGUN,					false },
-	{ ACT_HL2MP_IDLE_CROUCH,			ACT_HL2MP_IDLE_CROUCH_SHOTGUN,			false },
-	{ ACT_HL2MP_WALK_CROUCH,			ACT_HL2MP_WALK_CROUCH_SHOTGUN,			false },
-	{ ACT_HL2MP_GESTURE_RANGE_ATTACK,	ACT_HL2MP_GESTURE_RANGE_ATTACK_SHOTGUN,	false },
-	{ ACT_HL2MP_GESTURE_RELOAD,			ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,		false },
-	{ ACT_HL2MP_JUMP,					ACT_HL2MP_JUMP_SHOTGUN,					false },
-	{ ACT_RANGE_ATTACK1,				ACT_RANGE_ATTACK_SHOTGUN,				false },
+    { ACT_HL2MP_IDLE,					ACT_HL2MP_IDLE_SHOTGUN,					false },
+    { ACT_HL2MP_RUN,					ACT_HL2MP_RUN_SHOTGUN,					false },
+    { ACT_HL2MP_IDLE_CROUCH,			ACT_HL2MP_IDLE_CROUCH_SHOTGUN,			false },
+    { ACT_HL2MP_WALK_CROUCH,			ACT_HL2MP_WALK_CROUCH_SHOTGUN,			false },
+    { ACT_HL2MP_GESTURE_RANGE_ATTACK,	ACT_HL2MP_GESTURE_RANGE_ATTACK_SHOTGUN,	false },
+    { ACT_HL2MP_GESTURE_RELOAD,			ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,		false },
+    { ACT_HL2MP_JUMP,					ACT_HL2MP_JUMP_SHOTGUN,					false },
+    { ACT_RANGE_ATTACK1,				ACT_RANGE_ATTACK_SHOTGUN,				false },
 };
 IMPLEMENT_ACTTABLE( CZMWeaponShotgun );
 #endif
@@ -104,15 +90,9 @@ CZMWeaponShotgun::CZMWeaponShotgun()
 
     m_bFiresUnderwater = false;
 
-
-    m_bReloadsSingly = true;
-
 #ifndef CLIENT_DLL
     SetSlotFlag( ZMWEAPONSLOT_LARGE );
 #endif
-
-
-    m_bNeedPump = false;
 }
 
 void CZMWeaponShotgun::PrimaryAttack( void )
@@ -125,22 +105,22 @@ void CZMWeaponShotgun::PrimaryAttack( void )
     m_bNeedPump = true;
 
 
-	// MUST call sound before removing a round from the clip of a CMachineGun
-	WeaponSound(SINGLE);
+    // MUST call sound before removing a round from the clip of a CMachineGun
+    WeaponSound( SINGLE );
 
-	pPlayer->DoMuzzleFlash();
+    pPlayer->DoMuzzleFlash();
 
-	SendWeaponAnim( ACT_VM_PRIMARYATTACK );
+    SendWeaponAnim( ACT_VM_PRIMARYATTACK );
 
-	// Don't fire again until fire animation has completed
-	m_flNextPrimaryAttack = gpGlobals->curtime + SequenceDuration();
-	m_iClip1 -= 1;
+    // Don't fire again until fire animation has completed
+    m_flNextPrimaryAttack = gpGlobals->curtime + SequenceDuration();
+    m_iClip1 -= 1;
 
-	// player "shoot" animation
-	pPlayer->SetAnimation( PLAYER_ATTACK1 );
+    // player "shoot" animation
+    pPlayer->SetAnimation( PLAYER_ATTACK1 );
 
 
-	FireBulletsInfo_t info(
+    FireBulletsInfo_t info(
         7,
         pPlayer->Weapon_ShootPosition(),
         ((CBasePlayer*)pPlayer)->GetAutoaimVector( AUTOAIM_10DEGREES ),
@@ -148,63 +128,21 @@ void CZMWeaponShotgun::PrimaryAttack( void )
         MAX_TRACE_LENGTH,
         m_iPrimaryAmmoType );
 
-	info.m_pAttacker = pPlayer;
+    info.m_pAttacker = pPlayer;
     
-	// Fire the bullets, and force the first shot to be perfectly accuracy
-	FireBullets( info );
-	
-	QAngle punch;
-	punch.Init( SharedRandomFloat( "shotgunpax", -2, -1 ), SharedRandomFloat( "shotgunpay", -2, 2 ), 0 );
-	pPlayer->ViewPunch( punch );
+    // Fire the bullets, and force the first shot to be perfectly accuracy
+    FireBullets( info );
+    
+    QAngle punch;
+    punch.Init( SharedRandomFloat( "shotgunpax", -2, -1 ), SharedRandomFloat( "shotgunpay", -2, 2 ), 0 );
+    pPlayer->ViewPunch( punch );
 
-	if (!m_iClip1 && pPlayer->GetAmmoCount(m_iPrimaryAmmoType) <= 0)
-	{
-		// HEV suit - indicate out of ammo condition
-		pPlayer->SetSuitUpdate("!HEV_AMO0", FALSE, 0); 
-	}
+    if (!m_iClip1 && pPlayer->GetAmmoCount(m_iPrimaryAmmoType) <= 0)
+    {
+        // HEV suit - indicate out of ammo condition
+        pPlayer->SetSuitUpdate("!HEV_AMO0", FALSE, 0); 
+    }
 
 
     m_flNextPrimaryAttack = gpGlobals->curtime + GetFireRate();
-}
-
-void CZMWeaponShotgun::ItemPostFrame( void )
-{
-    if ( m_bNeedPump && (m_flNextPrimaryAttack <= gpGlobals->curtime) )
-    {
-        Pump();
-    }
-
-    BaseClass::ItemPostFrame();
-}
-
-void CZMWeaponShotgun::Pump()
-{
-    CZMPlayer* pOwner = GetPlayerOwner();
-
-    if ( !pOwner ) return;
-	
-    m_bNeedPump = false;
-
-    /*if ( m_bDelayedReload )
-    {
-	    m_bDelayedReload = false;
-	    StartReload();
-    }*/
-	
-    WeaponSound( SPECIAL1 );
-
-    // Finish reload animation
-    SendWeaponAnim( ACT_SHOTGUN_PUMP );
-
-    pOwner->m_flNextAttack	= gpGlobals->curtime + SequenceDuration();
-    m_flNextPrimaryAttack	= gpGlobals->curtime + SequenceDuration();
-}
-
-bool CZMWeaponShotgun::Reload( void )
-{
-    if ( m_bNeedPump ) return false;
-
-    if ( m_flNextPrimaryAttack > gpGlobals->curtime ) return false;
-
-    return BaseClass::Reload();
 }
