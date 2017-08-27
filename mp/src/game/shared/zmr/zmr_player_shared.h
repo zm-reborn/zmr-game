ffa483fa5ee52ef5897cb56ce27c3ee2249b5e7b
TotallyMehis <totally.mehis@gmail.com>
2017-08-27 15:58:35 +0300

    Unfucked player class

--- mp/src/game/shared/zmr/zmr_player_shared.h
+++ mp/src/game/shared/zmr/zmr_player_shared.h
@@ -1,14 +1,6 @@
 #pragma once
 
 
-#include "cbase.h"
-
-
-#ifdef CLIENT_DLL
-#define C_ZMPlayer CZMPlayer
-#else
-#define C_ZMPlayer CZMPlayer
-#endif
 
 #ifndef CLIENT_DLL
 #include "zmr/zmr_player.h"
@@ -16,16 +8,8 @@
 #include "zmr/c_zmr_player.h"
 #endif
 
-/*
-inline C_ZMPlayer* ToZMPlayer( CBaseEntity* pEntity )
-{
-	if ( !pEntity || !pEntity->IsPlayer() )
-		return NULL;
-
-	return dynamic_cast<CZMPlayer*>( pEntity );
-}
-
-inline C_ZMPlayer* ToZMPlayer( CBasePlayer* pPlayer )
-{
-	return static_cast<CZMPlayer*>( pPlayer );
-}*/
+#ifdef CLIENT_DLL
+#define CZMPlayer C_ZMPlayer
+#else
+#define C_ZMPlayer CZMPlayer
+#endif
