4c0d4065d7bf59d150c82524929755d68b4fddaa
TotallyMehis <totally.mehis@gmail.com>
2017-09-04 00:17:34 +0300

    Added trigger_blockphysexplosion

--- mp/src/game/shared/zmr/zmr_global_shared.cpp
+++ mp/src/game/shared/zmr/zmr_global_shared.cpp
@@ -18,6 +18,7 @@ CZMGlobalLists::~CZMGlobalLists()
 
 #ifndef CLIENT_DLL
     m_BlockHidden.Purge();
+    m_BlockPhysExp.Purge();
 #endif
 }
 
@@ -28,4 +29,5 @@ ZombieList_t* g_pZombies = &(g_pZMGlobals->m_Zombies);
 
 #ifndef CLIENT_DLL
 CUtlVector<CZMEntTriggerBlockHidden*>* g_pBlockHidden = &(g_pZMGlobals->m_BlockHidden);
+CUtlVector<CZMEntTriggerBlockPhysExp*>* g_pBlockPhysExp = &(g_pZMGlobals->m_BlockPhysExp);
 #endif
