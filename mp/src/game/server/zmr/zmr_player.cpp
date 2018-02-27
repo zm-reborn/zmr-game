623969b25b1e70a103df28485c4dc052021c3f6b
TotallyMehis <totally.mehis@gmail.com>
2018-02-27 20:08:56 +0200

    Remove old player model list

--- mp/src/game/server/zmr/zmr_player.cpp
+++ mp/src/game/server/zmr/zmr_player.cpp
@@ -75,30 +75,6 @@ LINK_ENTITY_TO_CLASS( player, CZMPlayer );
 PRECACHE_REGISTER( player );
 
 
-
-// ZMRTODO: Add support for loading models from file.
-const char* g_ZMPlayerModels[] = 
-{
-    "models/humans/group02/male_01.mdl",
-    "models/humans/group02/male_02.mdl",
-    "models/humans/group02/female_01.mdl",
-    "models/humans/group02/male_03.mdl",
-    "models/humans/group02/female_02.mdl",
-    "models/humans/group02/male_04.mdl",
-    "models/humans/group02/female_03.mdl",
-    "models/humans/group02/male_05.mdl",
-    "models/humans/group02/female_04.mdl",
-    "models/humans/group02/male_06.mdl",
-    "models/humans/group02/female_06.mdl",
-    "models/humans/group02/male_07.mdl",
-    "models/humans/group02/female_07.mdl",
-    "models/humans/group02/male_08.mdl",
-    "models/humans/group02/male_09.mdl",
-    "models/male_lawyer.mdl",
-    "models/male_pi.mdl",
-};
-
-
 CZMPlayerAnimState* CreateZMPlayerAnimState( CZMPlayer* pPlayer );
 
 CZMPlayer::CZMPlayer()
