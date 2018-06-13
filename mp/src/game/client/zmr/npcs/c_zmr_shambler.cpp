52d41a6afa7378b8a162ca054d9effbaf22fd7c1
Mehis <totally.mehis@gmail.com>
2018-06-13 17:05:42 +0300

    Properly bonemerge event hats

--- mp/src/game/client/zmr/npcs/c_zmr_shambler.cpp
+++ mp/src/game/client/zmr/npcs/c_zmr_shambler.cpp
@@ -8,152 +8,38 @@
 #define ZOMBIE_SANTAHAT     "models/props/misc/zombie_santahat01.mdl"
 
 
-extern ConVar zm_sv_happyzombies;
-ConVar zm_cl_happyzombies_disable( "zm_cl_happyzombies_disable", "0", 0, "No fun :(" );
-ConVar zm_cl_happyzombies_chance( "zm_cl_happyzombies_chance", "0.2", FCVAR_ARCHIVE );
-
-
 
 IMPLEMENT_CLIENTCLASS_DT( C_ZMShambler, DT_ZM_Shambler, CZMShambler )
 END_RECV_TABLE()
 
-// ZMRTODO: Put this somewhere nice.
-static int UTIL_CreateClientModel( const char* pszModel )
-{
-    int index;
-    index = modelinfo->GetModelIndex( pszModel );
-
-    if ( index == -1 )
-    {
-        // No model found, register our own.
-        index = modelinfo->RegisterDynamicModel( pszModel, true );
-    }
-    
-    return index;
-}
-
-// ZMRTODO: If we ever care enough, we need to re-do this completely.
-// This is not a good way of doing this.
-bool C_ZMHolidayHat::Initialize( C_BaseEntity* pOwner, const char* pszModel )
-{
-    int modelIndex = UTIL_CreateClientModel( pszModel );
-    if ( modelIndex == -1 )
-        return false;
-
-    if ( !InitializeAsClientEntityByIndex( modelIndex, RENDER_GROUP_OPAQUE_ENTITY ) )
-    {
-        return false;
-    }
-
-
-    SetOwnerEntity( pOwner );
-
-	SetNextClientThink( CLIENT_THINK_NEVER );
-
-    return true;
-}
-
-bool C_ZMHolidayHat::Parent( const char* pszAttachment )
-{
-    C_BaseEntity* pOwner = GetOwnerEntity();
-
-
-    int iAttachment = pOwner->LookupAttachment( pszAttachment );
-
-    if ( iAttachment <= 0 )
-        return false;
-
-
-    // Disables annoying asserts.
-    bool lastvalid = C_BaseEntity::IsAbsQueriesValid();
-    C_BaseEntity::SetAbsQueriesValid( true );
-
-    SetParent( pOwner, iAttachment );
-    SetAbsOrigin( pOwner->GetAbsOrigin() );
-    SetAbsAngles( vec3_angle );
-
-    C_BaseEntity::SetAbsQueriesValid( lastvalid );
-
-    SetLocalOrigin( vec3_origin );
-    SetLocalAngles( vec3_angle );
-
-    UpdateVisibility();
-
-    return true;
-}
 
 C_ZMShambler::C_ZMShambler()
 {
     SetZombieClass( ZMCLASS_SHAMBLER );
-    m_pHat = nullptr;
 }
 
 C_ZMShambler::~C_ZMShambler()
 {
-    ReleaseHat();
 }
 
-CStudioHdr* C_ZMShambler::OnNewModel()
+bool C_ZMShambler::CreateEventAccessories()
 {
-    CStudioHdr* hdr = BaseClass::OnNewModel();
-    
+    bool res = BaseClass::CreateEventAccessories();
 
-    if (zm_sv_happyzombies.GetInt() > 0
-    &&  !zm_cl_happyzombies_disable.GetBool()
-    &&  random->RandomFloat( 0.0f, 1.0f ) <= zm_cl_happyzombies_chance.GetFloat())
-    {
-        CreateHat();
-
+    if ( res )
         MakeHappy();
-    }
 
-    return hdr;
+    return res;
 }
 
-C_BaseAnimating* C_ZMShambler::BecomeRagdollOnClient()
+bool C_ZMShambler::IsAffectedByEvent( HappyZombieEvent_t iEvent ) const
 {
-    C_BaseAnimating* pRagdoll = BaseClass::BecomeRagdollOnClient();
-
-    ReleaseHat();
-
-    return pRagdoll;
+    return iEvent == HZEVENT_CHRISTMAS;
 }
 
-void C_ZMShambler::UpdateVisibility()
+const char* C_ZMShambler::GetEventHatModel( HappyZombieEvent_t iEvent ) const
 {
-    BaseClass::UpdateVisibility();
-
-    // Stay parented, silly.
-    if ( m_pHat )
-    {
-        m_pHat->UpdateVisibility();
-
-        if ( !IsDormant() )
-            m_pHat->Parent( "eyes" );
-    }
-}
-
-void C_ZMShambler::CreateHat()
-{
-    ReleaseHat();
-
-
-    m_pHat = new C_ZMHolidayHat();
-
-    // Add more hats here.
-    const char* model = nullptr;
-
-    switch ( zm_sv_happyzombies.GetInt() )
-    {
-    case 1 :
-    default : model = ZOMBIE_SANTAHAT; break;
-    }
-
-
-    if ( !m_pHat || !m_pHat->Initialize( this, model ) || !m_pHat->Parent( "eyes" ) )
-    {
-        ReleaseHat();
-    }
+    return ZOMBIE_SANTAHAT;
 }
 
 void C_ZMShambler::MakeHappy()
@@ -187,13 +73,4 @@ void C_ZMShambler::SetFlexWeightSafe( const char* pszName, float value )
 
 
     SetFlexWeight( cntrl, value );
-}
-
-void C_ZMShambler::ReleaseHat()
-{
-    if ( m_pHat )
-    {
-        m_pHat->Release();
-        m_pHat = nullptr;
-    }
 }
--- mp/src/game/client/zmr/npcs/c_zmr_zombiebase.h
+++ mp/src/game/client/zmr/npcs/c_zmr_zombiebase.h
@@ -5,6 +5,7 @@
 
 
 #include "zmr/c_zmr_charcircle.h"
+#include "c_zmr_hat.h"
 
 #include "zmr/zmr_player_shared.h"
 #include "zmr/zmr_shareddefs.h"
@@ -66,6 +67,17 @@ public:
     inline int  GetGroup() const { return m_iGroup; };
     inline void SetGroup( int group ) { m_iGroup = group; };
 
+
+    virtual CStudioHdr* OnNewModel() OVERRIDE;
+    virtual C_BaseAnimating* BecomeRagdollOnClient() OVERRIDE;
+    virtual void UpdateVisibility() OVERRIDE;
+protected:
+    virtual bool    CreateEventAccessories();
+    void            ReleaseHat();
+    virtual bool    IsAffectedByEvent( HappyZombieEvent_t iEvent ) const { return false; }
+    virtual const char* GetEventHatModel( HappyZombieEvent_t iEvent ) const { return ""; }
+
+
 protected:
     CZMCharCircle* m_fxHealth;
     CZMCharCircle* m_fxInner;
@@ -78,6 +90,9 @@ private:
     ZombieClass_t m_iZombieClass;
 
     QAngle m_angEyeAttachment;
+
+
+    C_ZMHolidayHat* m_pHat;
 };
 
 inline C_ZMBaseZombie* ToZMBaseZombie( C_BaseEntity* pEnt )
