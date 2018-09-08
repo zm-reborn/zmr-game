7675cbfbe30fe8dba4d58e321cac78127a794d12
Mehis <totally.mehis@gmail.com>
2018-09-08 15:01:12 +0300

    Add round restart cvars to options menu

--- mp/src/game/client/zmr/ui/zmr_options_misc.cpp
+++ mp/src/game/client/zmr/ui/zmr_options_misc.cpp
@@ -10,6 +10,12 @@ extern ConVar zm_cl_firstperson_deathcam;
 extern ConVar zm_cl_deathnotice_enabled;
 extern ConVar zm_cl_showhelp;
 extern ConVar zm_cl_fov;
+extern ConVar zm_cl_roundrestart_flashtaskbar;
+extern ConVar zm_cl_roundrestart_sound;
+
+
+static int RoundRestartCvarsToIndex();
+static void IndexToRoundRestartCvars( int index );
 
 
 CZMOptionsSubMisc::CZMOptionsSubMisc( Panel* parent ) : BaseClass( parent )
@@ -22,6 +28,7 @@ CZMOptionsSubMisc::CZMOptionsSubMisc( Panel* parent ) : BaseClass( parent )
     LoadItem( &m_pCheck_ShowHelp, "CheckShowHelp" );
     LoadItem( &m_pSlider_Fov, "SliderFov" );
     LoadItem( &m_pTextEntry_Fov, "FovEntry" );
+    LoadItem( &m_pCombo_FlashRR, "FlashRoundRestartCombo" );
 
     if ( FailedLoad() ) return;
 
@@ -32,6 +39,16 @@ CZMOptionsSubMisc::CZMOptionsSubMisc( Panel* parent ) : BaseClass( parent )
         m_pSlider_Fov->SetRange( fmin, fmax );
 
     m_pSlider_Fov->AddActionSignalTarget( this );
+
+
+    m_pCombo_FlashRR->AddItem( "None", nullptr );
+    m_pCombo_FlashRR->AddItem( "Play sound", nullptr );
+    m_pCombo_FlashRR->AddItem( "Flash taskbar", nullptr );
+    m_pCombo_FlashRR->AddItem( "Play sound + flash taskbar", nullptr );
+    
+#ifdef LINUX
+    m_pCombo_FlashRR->SetEnabled( false );
+#endif
 }
 
 CZMOptionsSubMisc::~CZMOptionsSubMisc()
@@ -47,6 +64,7 @@ void CZMOptionsSubMisc::OnApplyChanges()
     zm_cl_deathnotice_enabled.SetValue( m_pCheck_Deathnotice->IsSelected() ? 1 : 0 );
     zm_cl_showhelp.SetValue( m_pCheck_ShowHelp->IsSelected() ? 1 : 0 );
     zm_cl_fov.SetValue( m_pSlider_Fov->GetValue() );
+    IndexToRoundRestartCvars( m_pCombo_FlashRR->GetActiveItem() );
 }
 
 void CZMOptionsSubMisc::OnResetData()
@@ -58,6 +76,7 @@ void CZMOptionsSubMisc::OnResetData()
     m_pCheck_Deathnotice->SetSelected( zm_cl_deathnotice_enabled.GetBool() );
     m_pCheck_ShowHelp->SetSelected( zm_cl_showhelp.GetBool() );
     m_pSlider_Fov->SetValue( zm_cl_fov.GetInt() );
+    m_pCombo_FlashRR->ActivateItem( RoundRestartCvarsToIndex() );
 }
 
 void CZMOptionsSubMisc::OnSliderMoved( Panel* panel )
@@ -85,5 +104,48 @@ void CZMOptionsSubMisc::OnTextChanged( Panel* panel )
         m_pTextEntry_Fov->GetText( buffer, sizeof( buffer ) );
 
         m_pSlider_Fov->SetValue( atoi( buffer ) );
+    }
+}
+
+
+static int RoundRestartCvarsToIndex()
+{
+    if ( zm_cl_roundrestart_flashtaskbar.GetBool() && zm_cl_roundrestart_sound.GetBool() )
+    {
+        return 3;
+    }
+    else if ( zm_cl_roundrestart_flashtaskbar.GetBool() )
+    {
+        return 2;
+    }
+    else if ( zm_cl_roundrestart_sound.GetBool() )
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+static void IndexToRoundRestartCvars( int index )
+{
+    switch ( index )
+    {
+    case 0 :
+        zm_cl_roundrestart_flashtaskbar.SetValue( 0 );
+        zm_cl_roundrestart_sound.SetValue( 0 );
+        break;
+    case 1 :
+        zm_cl_roundrestart_flashtaskbar.SetValue( 0 );
+        zm_cl_roundrestart_sound.SetValue( 1 );
+        break;
+    case 2 :
+        zm_cl_roundrestart_flashtaskbar.SetValue( 1 );
+        zm_cl_roundrestart_sound.SetValue( 0 );
+        break;
+    case 3 :
+    default :
+        zm_cl_roundrestart_flashtaskbar.SetValue( 1 );
+        zm_cl_roundrestart_sound.SetValue( 1 );
+        break;
     }
 }
