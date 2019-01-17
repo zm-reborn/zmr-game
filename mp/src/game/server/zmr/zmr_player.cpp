9b905a64ac37930bc49dadc95393fa93eb90b69f
Mehis <totally.mehis@gmail.com>
2019-01-17 17:05:54 +0200

    Some activities and viewmodel sway pose parameter testing

--- mp/src/game/server/zmr/zmr_player.cpp
+++ mp/src/game/server/zmr/zmr_player.cpp
@@ -1332,7 +1332,7 @@ void CZMPlayer::CreateViewModel( int index )
     }
 
 
-    CBaseViewModel* vm = static_cast<CBaseViewModel*>( CreateEntityByName( "predicted_viewmodel" ) );
+    CZMViewModel* vm = static_cast<CZMViewModel*>( CreateEntityByName( "zm_viewmodel" ) );
     
     if ( vm )
     {
