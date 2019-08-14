e134c7507ed7b785968d8b87cc96323c6e2a6bcc
TotallyMehis <totally.mehis@gmail.com>
2019-08-14 12:33:03 +0300

    Sporting shotgun anims to rifle

--- mp/src/game/shared/zmr/weapons/zmr_shotgun_sporting.cpp
+++ mp/src/game/shared/zmr/weapons/zmr_shotgun_sporting.cpp
@@ -51,15 +51,15 @@ PRECACHE_WEAPON_REGISTER( weapon_zm_shotgun_sporting );
 
 acttable_t CZMWeaponShotgunSporting::m_acttable[] = 
 {
-    { ACT_MP_STAND_IDLE,				ACT_HL2MP_IDLE_SHOTGUN,                 false },
-    { ACT_MP_CROUCH_IDLE,				ACT_HL2MP_IDLE_CROUCH_SHOTGUN,          false },
-    { ACT_MP_RUN,					    ACT_HL2MP_RUN_SHOTGUN,                  false },
-    { ACT_MP_CROUCHWALK,			    ACT_HL2MP_WALK_CROUCH_SHOTGUN,          false },
-    { ACT_MP_ATTACK_STAND_PRIMARYFIRE,  ACT_HL2MP_GESTURE_RANGE_ATTACK_SHOTGUN, false },
-    { ACT_MP_ATTACK_CROUCH_PRIMARYFIRE,	ACT_HL2MP_GESTURE_RANGE_ATTACK_SHOTGUN, false },
-    { ACT_MP_RELOAD_STAND,			    ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,       false },
-    { ACT_MP_RELOAD_CROUCH,				ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,       false },
-    { ACT_MP_JUMP,					    ACT_HL2MP_JUMP_SHOTGUN,                 false },
+    { ACT_MP_STAND_IDLE,				ACT_HL2MP_IDLE_AR2,                 false },
+    { ACT_MP_CROUCH_IDLE,				ACT_HL2MP_IDLE_CROUCH_AR2,          false },
+    { ACT_MP_RUN,					    ACT_HL2MP_RUN_AR2,                  false },
+    { ACT_MP_CROUCHWALK,			    ACT_HL2MP_WALK_CROUCH_AR2,          false },
+    { ACT_MP_ATTACK_STAND_PRIMARYFIRE,  ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2, false },
+    { ACT_MP_ATTACK_CROUCH_PRIMARYFIRE,	ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2, false },
+    { ACT_MP_RELOAD_STAND,			    ACT_HL2MP_GESTURE_RELOAD_AR2,       false },
+    { ACT_MP_RELOAD_CROUCH,				ACT_HL2MP_GESTURE_RELOAD_AR2,       false },
+    { ACT_MP_JUMP,					    ACT_HL2MP_JUMP_AR2,                 false },
 };
 IMPLEMENT_ACTTABLE( CZMWeaponShotgunSporting );
 
