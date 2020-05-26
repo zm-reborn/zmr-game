14aa232e7cfdb9f5078cd85dddcbecede02f69ce
TotallyMehis <totally.mehis@gmail.com>
2020-05-26 20:16:10 +0300

    PBR shader from thexa4/source-pbr

--- mp/src/game/client/c_physicsprop.cpp
+++ mp/src/game/client/c_physicsprop.cpp
@@ -24,7 +24,11 @@ IMPLEMENT_CLIENTCLASS_DT(C_PhysicsProp, DT_PhysicsProp, CPhysicsProp)
 	RecvPropBool( RECVINFO( m_bAwake ) ),
 END_RECV_TABLE()
 
+#ifdef ZMR // ZMRCHANGE: PBR change
+ConVar r_PhysPropStaticLighting( "r_PhysPropStaticLighting", "0" );
+#else
 ConVar r_PhysPropStaticLighting( "r_PhysPropStaticLighting", "1" );
+#endif
 
 
 //-----------------------------------------------------------------------------
