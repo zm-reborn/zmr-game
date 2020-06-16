9684645fd0abaf1776b76724a52a89446d46dcc7
TotallyMehis <totally.mehis@gmail.com>
2020-06-16 16:01:46 +0300

    Fix transient nav areas unblocking itself (#314)

--- mp/src/game/server/zmr/nav/zmr_nav_area.cpp
+++ mp/src/game/server/zmr/nav/zmr_nav_area.cpp
@@ -24,6 +24,81 @@ void CZMRNavArea::ComputeSpotEncounters()
 {
 }
 
+void CZMRNavArea::UpdateBlocked( bool force, int teamID )
+{
+    //if ( !force && m_BlockedTimer.IsElapsed() )
+    //{
+    //    return;
+    //}
+
+    // Transient areas are updated separately.
+    if ( HasAttributes( NAV_MESH_TRANSIENT ) )
+    {
+        return;
+    }
+
+
+    BaseClass::UpdateBlocked( force, teamID );
+
+
+    /*m_BlockedTimer.Start( 1.0f );
+
+    CZMNavTransientFilter filter;
+    Vector mins, maxs, center;
+
+
+    GetBlockTraceBox( center, mins, maxs );
+
+    trace_t tr;
+        
+    UTIL_TraceHull( center, center, mins, maxs, MASK_SOLID, &filter, &tr );
+
+
+    bool bWasBlocked = IsBlocked( TEAM_ANY );
+    bool bBlocked = tr.fraction != 1.0f || tr.startsolid || tr.m_pEnt;
+    bool bChanged = bBlocked != bWasBlocked;
+
+
+
+
+	if ( bChanged )
+	{
+        for ( int i = 0; i < 2; i++ )
+        {
+            m_isBlocked[i] = bBlocked;
+        }
+
+        auto* pEvent = gameeventmanager->CreateEvent( "nav_blocked" );
+		if ( pEvent )
+		{
+			pEvent->SetInt( "area", GetID() );
+			pEvent->SetInt( "blocked", bBlocked );
+			gameeventmanager->FireEvent( pEvent );
+		}
+
+		if ( bBlocked )
+		{
+			TheNavMesh->OnAreaBlocked( this );
+		}
+		else
+		{
+			TheNavMesh->OnAreaUnblocked( this );
+		}
+	}
+
+	if ( TheNavMesh->GetMarkedArea() == this )
+	{
+		if ( IsBlocked( teamID ) )
+		{
+			NDebugOverlay::Box( center, mins, maxs, 255, 0, 0, 64, 3.0f );
+		}
+		else
+		{
+			NDebugOverlay::Box( center, mins, maxs, 0, 255, 0, 64, 3.0f );
+		}
+	}*/
+}
+
 bool CZMRNavArea::IsBlocked( int teamID, bool ignoreNavBlockers ) const
 {
     return BaseClass::IsBlocked( teamID, ignoreNavBlockers ) || m_bHasNoFloor;
--- mp/src/game/server/zmr/nav/zmr_nav_area.h
+++ mp/src/game/server/zmr/nav/zmr_nav_area.h
@@ -16,7 +16,7 @@ public:
     virtual void ComputeHidingSpots() OVERRIDE;
     virtual void ComputeSniperSpots() OVERRIDE;
     virtual void ComputeSpotEncounters() OVERRIDE;
-
+    virtual void UpdateBlocked( bool force = false, int teamID = TEAM_ANY ) OVERRIDE;
 
     virtual bool IsBlocked( int teamID, bool ignoreNavBlockers = false ) const OVERRIDE;
     void SetNoFloor( bool state ) { m_bHasNoFloor = state; }
