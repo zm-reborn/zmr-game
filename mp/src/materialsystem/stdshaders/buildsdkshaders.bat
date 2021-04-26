15b96a7a01e2adc027ab2d6a4adb0b85d31358ec
Miikka Ylätalo <totally.mehis@gmail.com>
2021-04-26 16:37:16 +0300

    Use SCell555's ShaderCompile instead of perl crap

--- mp/src/materialsystem/stdshaders/buildsdkshaders.bat
+++ mp/src/materialsystem/stdshaders/buildsdkshaders.bat
@@ -26,7 +26,7 @@ set BUILD_SHADER=call buildshaders.bat
 set ARG_EXTRA=
 
 %BUILD_SHADER% stdshader_dx9_20b		-game %GAMEDIR% -source %SOURCEDIR%
-%BUILD_SHADER% stdshader_dx9_30			-game %GAMEDIR% -source %SOURCEDIR% -dx9_30	-force30 
+%BUILD_SHADER% stdshader_dx9_30			-game %GAMEDIR% -source %SOURCEDIR% -force30
 
 
 rem echo.
